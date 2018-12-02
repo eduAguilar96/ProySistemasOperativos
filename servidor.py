e4a1645ff7b4b9fa4e7a350b96eec9606ee24979
eduAguilar96 <eduxx77@gmail.com>
2018-12-02 13:53:38 -0600

    Create Queue class

--- servidor.py
+++ servidor.py
@@ -1,10 +1,33 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#This sample program, based on the one in the standard library documentation, receives incoming messages and echos them back to the sender. It starts by creating a TCP/IP socket.
-
 import socket
 import sys
 import time
+
+class Queue:
+  #Constructor creates a list
+  def __init__(self):
+      self.queue = list()
+
+  #Adding elements to queue
+  def enqueue(self,data):
+      #Checking to avoid duplicate entry (not mandatory)
+      if data not in self.queue:
+          self.queue.insert(0,data)
+          return True
+      return False
+
+  #Removing the last element from the queue
+  def dequeue(self):
+      if len(self.queue)>0:
+          return self.queue.pop()
+      return ("Queue Empty!")
+
+  #Getting the size of the queue
+  def size(self):
+      return len(self.queue)
+
+  #printing the elements of the queue
+  def printQueue(self):
+      return self.queue
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
