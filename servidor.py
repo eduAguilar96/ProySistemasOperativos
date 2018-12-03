8cc0e58358923d5d9725592966a91f1641471cb1
eduAguilar96 <eduxx77@gmail.com>
2018-12-02 20:03:47 -0600

    Implement Quantum Logic

--- servidor.py
+++ servidor.py
@@ -68,83 +68,127 @@ pQueue = Queue() #cola de listos
 CPU = "L"
 processID = 1
 processSize = ["L"]
+lastPageUsed = [0]
 
 def incrementTimestamp(time):
-	global timestamp
-	timestamp += time
+    global timestamp
+    timestamp += time
 
 def resetDelta():
-	global delta
-	delta = quantum
+    global delta
+    delta = quantum
 
 def decrementDelta():
-	global delta
-	delta -= quantum
+    global delta
+    delta -= quantum
 
 def addPage(processID, pageID):
-	global pageTable
-	# processName = str(processID) + "."
-	# processMatching = [s for s in pageTable if processName in s]
-	proccessWithPage = str(processID) + "." + str(int(pageID))
-	if proccessWithPage in pageTable:
-		#nothing, page already loaded
-		print("Page already in Real Memory")
+    # processName = str(processID) + "."
+    # processMatching = [s for s in pageTable if processName in s]
+    global pageTable
+    global lastPageUsed
+    lastPageUsed[int(processID)] = int(pageID)
+    proccessWithPage = str(processID) + "." + str(int(pageID))
+    if proccessWithPage in pageTable:
+       #nothing, page already loaded
+       print("Page already in Real Memory")
 
-	else:
-		print("Page Fault in Real Memory")
-		#memoria libre
-		if "L" in pageTable:
-			i = pageTable.index("L")
-			pageTable[i] = proccessWithPage
-		#memoria ocpada, necesidad de un swap
-		else:
-			print("Swap in page table, PENDING LOGIC")
-	return str(pageTable.index(proccessWithPage)) + ":" + proccessWithPage
+    else:
+        print("Page Fault in Real Memory")
+        #memoria libre
+        if "L" in pageTable:
+            i = pageTable.index("L")
+            pageTable[i] = proccessWithPage
+            #memoria ocpada, necesidad de un swap
+        else:
+            print("Swap in page table, PENDING LOGIC")
+
+        return str(pageTable.index(proccessWithPage)) + ":" + proccessWithPage
+
+def addSwapPage(processID, pageID):
+    global swapTable
+    proccessWithPage = str(processID) + "." + str(int(pageID))
+    if proccessWithPage in swapTable:
+        # nothing, page already loaded
+        print("Page already in Swap Memory")
+
+    else:
+        print("Page Fault in Swap Memory")
+        # memoria libre
+        if "L" in swapTable:
+            i = swapTable.index("L")
+            swapTable[i] = proccessWithPage
+        # memoria ocpada, necesidad de un swap
+        else:
+            print("Swap in swap table, PENDING LOGIC")
+    return str(swapTable.index(proccessWithPage)) + ":" + proccessWithPage
 
 def addQueueProcToCPU():
-	global CPU
-	topProcessID = pQueue.dequeue()
-	tableEntry = addPage(topProcessID, 0)
-	CPU = (topProcessID)
+    global CPU
+    topProcessID = pQueue.dequeue()
+    tableEntry = addPage(topProcessID, lastPageUsed[topProcessID])
+    CPU = (topProcessID)
 
 def create(size):
-	global processID
-	global CPU
-	global processSize
-	processSize.append(int(math.ceil(float(size))))
-	pQueue.enqueue(processID)
+    # global pQueue
+    global processID
+    global CPU
+    global processSize
+    global lastPageUsed
+    processSize.append(int(math.ceil(float(size))))
+    pQueue.enqueue(processID)
+    lastPageUsed.append(0)
 
-	#if first process
-	if CPU == "L":
-		addQueueProcToCPU()
 
-	print >>sys.stderr, 'sending answer back to the client'
-	answer = "%.3f process %s created size %s pages" % (timestamp, processID, processSize[processID])
-	connection.sendall(answer)
-	processID += 1
+    #if first process
+    if CPU == "L":
+        addQueueProcToCPU()
+
+    print >>sys.stderr, 'sending answer back to the client'
+    answer = "%.3f process %s created size %s pages" % (timestamp, processID, processSize[processID])
+    connection.sendall(answer)
+    processID += 1
 
 def address(processID, virtualAddress):
-	#process not in CPU
-	if (processID) not in str(CPU):
-		message = "%.3f ERROR: Requested process ID not in CPU" % timestamp
-		print(message)
-		connection.sendall(message)
-		return None
-	#virtual address out of bounds
-	if (processSize[int(processID)]*pageSizeInBytes) <= int(virtualAddress):
-		message = "%.3f ERROR: Requested virtul address out of bounds" % timestamp
-		print(message)
-		connection.sendall(message)
-		return None
-	procPageID = math.floor(int(virtualAddress)/pageSizeInBytes)
-	procByteNum = int(virtualAddress)%pageSizeInBytes
-	tableEntry = addPage(processID, procPageID)
-	pageEntry = (tableEntry.split(":"))[0]
-	realAddress = int(pageEntry)*pageSizeInBytes + int(procByteNum)
-	print >>sys.stderr, 'sending answer back to the client'
-	answer = "%.3f real address: %s" % (timestamp, realAddress)
-	connection.sendall(answer)
+    #process not in CPU
+    if (processID) not in str(CPU):
+        message = "%.3f ERROR: Requested process ID not in CPU" % timestamp
+        print(message)
+        connection.sendall(message)
+        return None
+        #virtual address out of bounds
+    if (processSize[int(processID)]*pageSizeInBytes) <= int(virtualAddress):
+        message = "%.3f ERROR: Requested virtul address out of bounds" % timestamp
+        print(message)
+        connection.sendall(message)
+        return None
+    procPageID = math.floor(int(virtualAddress)/pageSizeInBytes)
+    procByteNum = int(virtualAddress)%pageSizeInBytes
+    tableEntry = addPage(processID, procPageID)
+    pageEntry = (tableEntry.split(":"))[0]
 
+    realAddress = int(pageEntry)*pageSizeInBytes + int(procByteNum)
+
+    print >>sys.stderr, 'sending answer back to the client'
+    answer = "%.3f real address: %s" % (timestamp, realAddress)
+    connection.sendall(answer)
+
+def quantumFunc():
+    global queue
+    incrementTimestamp(float(quantum))
+
+    if CPU != "L" and pQueue.size() != 0:
+        #quitar proceso del CPU y poner otro
+        addSwapPage(int(CPU), lastPageUsed[int(CPU)])
+        pQueue.enqueue(CPU)
+        addQueueProcToCPU()
+    elif pQueue.size() != 0:
+        #poner proceso en el CPU
+        addQueueProcToCPU()
+
+    print >>sys.stderr, 'sending answer back to the client'
+    answer = "%.3f quantum end" % timestamp
+    connection.sendall(answer)
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -170,45 +214,45 @@ connection, client_address = sock.accept()
 #accept() returns an open connection between the server and client, along with the address of the client. The connection is actually a different socket on another port (assigned by the kernel). Data is read from the connection with recv() and transmitted with sendall().
 
 try:
-	print >>sys.stderr, 'connection from', client_address
+    print >>sys.stderr, 'connection from', client_address
 
     # Receive the data
-	while True:
-		data = connection.recv(256)
-		if data == "":
-			print >>sys.stderr, 'no data from', client_address
-			connection.close()
-			sys.exit()
-		command = data
-		comment = None
-		parameters = []
+    while True:
+        data = connection.recv(256)
+        if data == "":
+            print >>sys.stderr, 'no data from', client_address
+            connection.close()
+            sys.exit()
+        command = data
+        comment = None
+        parameters = []
 
-		if "//" in command:
-			commentSplit = data.split("//")
-			command = commentSplit[0]
-			comment = commentSplit[1]
-		if " " in  command:
-			parameters = commentSplit[0].split(" ")
-			command = parameters[0]
+        if "//" in command:
+            commentSplit = data.split("//")
+            command = commentSplit[0]
+            comment = commentSplit[1]
+        if " " in  command:
+            parameters = commentSplit[0].split(" ")
+            command = parameters[0]
 
-		incrementTimestamp(0.001)
-		#Create %s, size in pages
-		if command == "Create":
-			create(parameters[1])
-		#Quantum
-		if command == "Quantum":
-			print("Quantum")
-		#Address
-		if command == "Address":
-			address(parameters[1], parameters[2])
-		#Fin
-		#End
-
+        incrementTimestamp(0.001)
+        #Create %s, size in pages
+        if command == "Create":
+            create(parameters[1])
+        #Quantum
+        if command == "Quantum":
+            quantumFunc()
+        #Address
+        if command == "Address":
+            address(parameters[1], parameters[2])
+        #Fin
+        #End
+        print(pQueue.printQueue())
 
 finally:
-     # Clean up the connection
-	print >>sys.stderr, 'se fue al finally'
-	connection.close()
+    # Clean up the connection
+    print >>sys.stderr, 'se fue al finally'
+    connection.close()
 
 #When communication with a client is finished, the connection needs to be cleaned up using close(). This example uses a try:finally block to ensure that close() is always called, even in the event of an error.
 
